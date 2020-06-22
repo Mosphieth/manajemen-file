8f4322bbac98fae01c3d32aaf5a0a5e3e7b71bca
Mosphieth <Mosphieth@noreply.github.com>
2020-06-22 13:07:27 +0700

    little change on side bar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -2,12 +2,12 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <span class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
         <div class="sidebar-brand-icon">
             <i class="fas fa-file"></i>
         </div>
         <div class="sidebar-brand-text mx-3">Manajemen File</div>
-    </a>
+    </span>
 
     <!-- Divider -->
     <hr class="sidebar-divider">
